3e75abfbc53ce878c16af218a46509c0ee04e520
JCMiriam <myriamkey612@gmail.com>
2026-08-19 13:21:02 +0200

    feat: add not found view

---
+++ src/common/views/NotFoundView.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="not-found">
+    <p class="not-found__code">404</p>
+    <h1>Esta mesa no está reservada</h1>
+    <p>La página que buscas no existe o ha cambiado de sitio.</p>
+    <RouterLink to="/">Volver al plan semanal</RouterLink>
+  </section>
+</template>
+
+<style scoped src="./NotFoundView.styles.scss" lang="scss"></style>
